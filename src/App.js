a77ba24e40569495de7f1c08e08e5d80b7b21c1c
Beth Evans <baevans_87@hotmail.com>
2017-09-13 19:02:20 -0500

    map working removed unnecessary script files

--- src/App.js
+++ src/App.js
@@ -2,31 +2,58 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class Header extends Component {
   render() {
     return (
-      <div className="App">
+      <div className='page'>
+        <div className='navbar navbar-inverse'>
+          <div className='container'>
+            <a href='/' className="navbar-brand">
+              The Baseball Jedi
+            </a>
+            <Navigation />
 
-        <div className="App-header">
-          <ul className='nav nav-tabs'>
-            <li><a href='/'>MLB</a></li>
-            <li><a href='/'>Prospect Watch</a></li>
-            <li><a href='/'>Fantasy Baseball</a></li>
-            <li><a href='/'>Stadiums</a></li>
-          </ul>
+          </div>
         </div>
-
-        <div className='container'>
-          <a href='/' className="App-intro navbar-brand">
-            The Baseball Jedi
-          </a>
-        </div>
-
+        <Logo />
+        <StadiumMap />
       </div>
+    );
+  }
+}
+class Logo extends Component {
+  render() {
+    return (
+      <div className='vertical-center'>
+        <div className='col-md-4 col-md-offset-4'>
+          <h1 className='text-center myHeader'>
+            <a href='/'>The Baseball Jedi</a>
+          </h1>
+        </div>
+      </div>
+    );
+  }
+}
+class Navigation extends Component {
+  render() {
+    return (
+      <ul className='nav navbar-nav navbar-right'>
+        <li><a href='/'>MLB</a></li>
+        <li><a href='/'>Prospect Watch</a></li>
+        <li><a href='/'>Fantasy Baseball</a></li>
+        <li><a href='/'>Stadiums</a></li>
+      </ul>
+    );
+  }
+}
+class StadiumMap extends Component {
+  render() {
+    return (
+      <div id='map'></div>
     );
   }
 }
 
 
 
-export default App;
+export default Header;
