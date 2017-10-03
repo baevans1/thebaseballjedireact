04e82c2661bb4ad5c8a29c6ab865df6bb803c292
Beth Evans <baevans_87@hotmail.com>
2017-10-02 21:24:11 -0500

    routing added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+
+
 
 class Header extends Component {
   render() {
@@ -8,15 +10,19 @@ class Header extends Component {
       <div className='page'>
         <div className='navbar navbar-inverse'>
           <div className='container'>
-            <a href='/' className="navbar-brand">
+            <Link to='/' className="navbar-brand">
               The Baseball Jedi
-            </a>
-            <Navigation />
-
+            </Link>
+            <Navigation />    
           </div>
         </div>
-        <Logo />
-        <StadiumMap />
+        <Switch>
+          <Route exact path='/' component={Logo}/>
+          <Route path='/MajorLeague' component={StadiumMap}/>
+          <Route path='/ProspectWatch' component={StadiumMap}/>
+          <Route path='/FantasyBaseball' component={StadiumMap}/>
+          <Route path='/Stadiums' component={StadiumMap}/>
+        </Switch>
       </div>
     );
   }
@@ -38,10 +44,10 @@ class Navigation extends Component {
   render() {
     return (
       <ul className='nav navbar-nav navbar-right'>
-        <li><a href='/'>MLB</a></li>
-        <li><a href='/'>Prospect Watch</a></li>
-        <li><a href='/'>Fantasy Baseball</a></li>
-        <li><a href='/'>Stadiums</a></li>
+        <li><Link to='/MajorLeague'>MLB</Link></li>
+        <li><Link to='/ProspectWatch'>Prospect Watch</Link></li>
+        <li><Link to='/FantasyBaseball'>Fantasy Baseball</Link></li>
+        <li><Link to='/Stadiums'>Stadiums</Link></li>
       </ul>
     );
   }
